37ded1d9868f5abbdb23c5bb9f20ed150bff5a95
JC <jcfisher007@gmail.com>
2018-10-31 09:42:30 -0700

    v1.1.0

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Adds ctx.log with structured json loadding based on roarr.
 
 ## Example:
 
-```
+```js
 var App = require("koa");
 var log = require("roarr");
 var logMiddleware = require("koa-roarr");
 var app = new App();
-
 app.use(logMiddleware({ log }));
+```
+
+### Custom messages
+
+```js
+app.use(logMiddleware({ log, inMsg: "start", outMsg: "complete" }));
 ```
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var uuidv4 = require("uuid/v4");
 
-var logger = ({ log }) => async (ctx, next) => {
+var logger = ({
+  log,
+  inMsg = "http request",
+  outMsg = "http response"
+}) => async (ctx, next) => {
   var { request, response } = ctx;
   var requestId = ctx.request.headers["x-request-id"] || `s-${uuidv4()}`;
   ctx.log = log.child({ requestId });
+  ctx.log.info({ request }, inMsg);
   await next();
-  ctx.log.info({ request, response }, "request complete");
+  ctx.log.info({ response }, outMsg);
 };
 
 module.exports = logger;
